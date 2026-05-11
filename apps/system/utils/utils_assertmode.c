e2c2b59625b36adacc581744b7a5088a0c056ef1
jijo.jacob <jijo.jacob@samsung.com>
2026-05-11 14:29:12 +0530

    apps/system/utils: Add assertmode TASH command for runtime assert behavior control
    This commit adds a TASH command to control assert behavior at runtime
    through the assertmode driver interface.
    
    Command Usage:
      TASH>> assertmode          # Show current mode
      TASH>> assertmode 0        # Set auto-reset mode
      TASH>> assertmode 1        # Set halt mode (with watchdog kick)
      TASH>> assertmode 2        # Set not-selected mode
    
    Files changed:
    - apps/include/system/utils.h: Add utils_assertmode() declaration
    - apps/system/utils/Kconfig: Add CONFIG_ENABLE_ASSERTMODE option
    - apps/system/utils/Makefile: Build utils_assertmode.c when enabled
    - apps/system/utils/systemcmd.c: Register TASH command
    - apps/system/utils/utils_assertmode.c: Command implementation
    
    Configuration:
      CONFIG_ENABLE_ASSERTMODE=y

---
+++ apps/system/utils/utils_assertmode.c
@@ -0,0 +1,122 @@
+/****************************************************************************
+ * apps/system/utils/utils_assertmode.c
+ *
+ *   Copyright (C) 2026 Samsung Electronics. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <tinyara/config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <tinyara/assertmode.h>
+
+#if defined(CONFIG_ENABLE_ASSERTMODE)
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+int utils_assertmode(int argc, char **args)
+{
+	int fd;
+	int ret;
+	struct assertmode_info_s info;
+
+	fd = open(ASSERTMODE_DRVPATH, O_RDWR);
+	if (fd < 0) {
+		printf("ERROR: Failed to open %s, errno: %d\n",
+		       ASSERTMODE_DRVPATH, get_errno());
+		return ERROR;
+	}
+
+	printf("\n");
+
+	if (argc == 1) {
+		/* No argument: read and display current mode */
+		ret = ioctl(fd, ASSERTMODEIOC_GET_MODE, (unsigned long)&info);
+		if (ret < 0) {
+			printf("ERROR: ioctl GET_MODE failed, errno: %d\n", get_errno());
+			close(fd);
+			return ERROR;
+		}
+
+		if (info.mode == ASSERT_BEHAVIOR_AUTORESET) {
+			printf("Current: auto-reset\n");
+		} else if (info.mode == ASSERT_BEHAVIOR_HALT) {
+			printf("Current: halt (watchdog kick)\n");
+		} else {
+			printf("Current: assert behavior is not-selected \n");
+		}
+	} else if (argc == 2) {
+		/* Set mode */
+		int mode = atoi(args[1]);
+
+		if (mode < ASSERT_BEHAVIOR_AUTORESET || mode > ASSERT_BEHAVIOR_NOT_SELECTED) {
+			printf("Invalid mode. Use 0 (auto-reset), 1 (halt), or 2 (not-selected)\n");
+			close(fd);
+			return ERROR;
+		}
+
+		info.mode = mode;
+		ret = ioctl(fd, ASSERTMODEIOC_SET_MODE, (unsigned long)&info);
+		if (ret < 0) {
+			printf("ERROR: ioctl SET_MODE failed, errno: %d\n", get_errno());
+			printf("\n");
+			close(fd);
+			return ERROR;
+		}
+
+		if (mode == ASSERT_BEHAVIOR_AUTORESET) {
+			printf("Assert behavior set to auto-reset\n");
+		} else if (mode == ASSERT_BEHAVIOR_HALT) {
+			printf("Assert behavior set to halt (watchdog kick)\n");
+		} else {
+			printf("Assert behavior is not-selected\n");
+		}
+	}
+
+	printf("Usage: assertmode [0|1|2]\n");
+	printf("  assertmode (for assertmode status)\n");
+	printf("  0 = auto-reset on assert\n");
+	printf("  1 = halt with watchdog kick on assert\n");
+	printf("  2 = assert behavior is not-selected\n");
+
+	close(fd);
+	return OK;
+}
+
+#endif /* CONFIG_ENABLE_ASSERTMODE */
